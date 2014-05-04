259f6370498478659061179d3e2e872a32970f4a
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-03 23:02:39 -0500

    More frequent progress updates even if --precise-progress is not active

--- src/merge/output_control.cpp
+++ src/merge/output_control.cpp
@@ -2180,9 +2180,7 @@ pull_packetizers_for_packets() {
            && (FILE_STATUS_MOREDATA == ptzr.status)
            && !ptzr.packetizer->packet_available()) {
       ptzr.status = ptzr.packetizer->read();
-
-      if (g_precise_progress)
-        display_progress();
+      display_progress();
     }
 
     if (   (FILE_STATUS_MOREDATA != ptzr.status)
