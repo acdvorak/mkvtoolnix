dfa4d8329791ccd70fd33df2625080079600e834
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-07 00:41:35 -0500

    Return file size-based progress values in SubRip Text reader

--- src/input/r_srt.cpp
+++ src/input/r_srt.cpp
@@ -14,6 +14,7 @@
 #include "common/common_pch.h"
 
 #include "common/codec.h"
+#include "common/math.h"
 #include "input/r_srt.h"
 #include "input/subtitles.h"
 
@@ -73,9 +74,12 @@ srt_reader_c::read(generic_packetizer_c *,
 
 progress_c
 srt_reader_c::get_progress() {
-  int num_entries = m_subs->get_num_entries();
-
-  return progress_c{m_subs->get_num_processed(), num_entries};
+  auto file_size      = m_size;
+  auto units_read     = m_subs->get_num_processed();
+  auto units_per_file = m_subs->get_num_entries();
+  double scale = 1.0 * file_size / units_per_file;
+  int64_t bytes_done = irnd(units_read * scale);
+  return PROGRESS_C(bytes_done, file_size);
 }
 
 void
